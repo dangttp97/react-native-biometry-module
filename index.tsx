4668539d347c6c4878880aa57a1a6ab88c3bc211
Trần Thành Phương Đăng <phuongdang012@gmail.com>
2022-04-08 15:58:34 +0700

    Export needed type

--- index.tsx
+++ index.tsx
@@ -1,69 +1,69 @@
-import React, { PureComponent } from 'react'
-import { InputPasscode, Locked, SelectPasscode } from '@screens'
+import React, { PureComponent } from "react";
+import { InputPasscode, Locked, SelectPasscode } from "@screens";
 import {
   deletePinCode,
   hasPinCode,
   PasscodeResultStatus,
   PasscodeType,
   resetInternalStates,
-} from 'commons'
-import { StyleProp, ViewStyle } from 'react-native'
+} from "@commons";
+import { StyleProp, ViewStyle } from "react-native";
 
 export interface BiometryProps {
-  type: PasscodeType
-  alphabetCharsVisible: boolean
-  biometricDisabled: boolean
-  passcodeVisible?: boolean
+  type: PasscodeType;
+  alphabetCharsVisible: boolean;
+  biometricDisabled: boolean;
+  passcodeVisible?: boolean;
 
-  timePasscodeLockedAsyncStorageName?: string
-  passcodeKeychainName?: string
-  passcodeAttemptsAsyncStorageName?: string
+  timePasscodeLockedAsyncStorageName?: string;
+  passcodeKeychainName?: string;
+  passcodeAttemptsAsyncStorageName?: string;
 
-  passcodeStatus?: PasscodeResultStatus
+  passcodeStatus?: PasscodeResultStatus;
 
-  lockedButton?: JSX.Element
-  lockedPage?: JSX.Element
-  bottomLeftButton?: JSX.Element
+  lockedButton?: JSX.Element;
+  lockedPage?: JSX.Element;
+  bottomLeftButton?: JSX.Element;
 
-  onSuccess?: (passcode: string) => void
-  onFailed?: (error?: any) => void
+  onSuccess?: (passcode: string) => void;
+  onFailed?: (error?: any) => void;
 
-  styleContainer?: StyleProp<ViewStyle>
+  styleContainer?: StyleProp<ViewStyle>;
 }
 
 export interface BiometryState {
-  internalPasscodeStatus: PasscodeResultStatus
-  passcodeLocked: boolean
+  internalPasscodeStatus: PasscodeResultStatus;
+  passcodeLocked: boolean;
 }
 
-const passcodeKeychainNameDefault = 'PasscodeKeychainDefault'
-const passcodeAttemptsAsyncStorageNameDefault = 'PasscodeAttemptsDefault'
-const timePasscodeLockedAsyncStorageNameDefault = 'TimePasscodeLockedDefault'
+const passcodeKeychainNameDefault = "PasscodeKeychainDefault";
+const passcodeAttemptsAsyncStorageNameDefault = "PasscodeAttemptsDefault";
+const timePasscodeLockedAsyncStorageNameDefault = "TimePasscodeLockedDefault";
 
-export class Biometry extends PureComponent<BiometryProps, BiometryState> {
+class Biometry extends PureComponent<BiometryProps, BiometryState> {
   constructor(props: BiometryProps) {
-    super(props)
+    super(props);
     this.state = {
       internalPasscodeStatus: PasscodeResultStatus.initial,
       passcodeLocked: false,
-    }
-    this.changeInternalStatus = this.changeInternalStatus.bind(this)
+    };
+    this.changeInternalStatus = this.changeInternalStatus.bind(this);
   }
 
   changeInternalStatus(status: PasscodeResultStatus) {
     if (status === PasscodeResultStatus.initial) {
-      this.setState({ passcodeLocked: false })
+      this.setState({ passcodeLocked: false });
     }
 
-    this.setState({ internalPasscodeStatus: status })
+    this.setState({ internalPasscodeStatus: status });
   }
 
   renderLockedScreen() {
-    return <Locked buttonComponent={this.props.lockedButton} />
+    return <Locked buttonComponent={this.props.lockedButton} />;
   }
 
   render() {
-    const { type, passcodeStatus } = this.props
+    const { type, passcodeStatus } = this.props;
 
     return (
       <>
@@ -97,24 +97,33 @@ export class Biometry extends PureComponent<BiometryProps, BiometryState> {
             ? this.props.lockedPage
             : this.renderLockedScreen())}
       </>
-    )
+    );
   }
 }
 
-export function hasUserSetPasscode(serviceName?: string) {
-  return hasPinCode(serviceName || passcodeKeychainNameDefault)
-}
+const hasUserSetPasscode = (serviceName?: string) => {
+  return hasPinCode(serviceName || passcodeKeychainNameDefault);
+};
 
-export function deleteUserPasscode(serviceName?: string) {
-  return deletePinCode(serviceName || passcodeKeychainNameDefault)
-}
+const deleteUserPasscode = (serviceName?: string) => {
+  return deletePinCode(serviceName || passcodeKeychainNameDefault);
+};
 
-export function resetPasscodeInternalStates(
+const resetPasscodeInternalStates = (
   passcodeAttempsStorageName?: string,
-  timePasscodeLockedStorageName?: string,
-) {
+  timePasscodeLockedStorageName?: string
+) => {
   return resetInternalStates([
     passcodeAttempsStorageName || passcodeAttemptsAsyncStorageNameDefault,
     timePasscodeLockedStorageName || timePasscodeLockedAsyncStorageNameDefault,
-  ])
-}
+  ]);
+};
+
+export {
+  PasscodeType,
+  PasscodeResultStatus,
+  Biometry,
+  hasUserSetPasscode,
+  deleteUserPasscode,
+  resetPasscodeInternalStates,
+};
